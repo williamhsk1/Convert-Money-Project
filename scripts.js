567124424eeb7e5ba21a93bca475e6822040819a
William Cruz <williamfelipesoares_445@hotmail.com>
2022-03-12 17:08:10 -0300

    Adding Chinese currency

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const button = document.getElementById("convert-button");
 const select = document.getElementById("currency-select");
 
-const dolar = 5.2;
-const euro = 5.9;
+const dolar = 5.0750;
+const euro = 5.5374;
 const bitcoin = 0.0000052;
+const renminbi = 0.8005;
 
 const convertValues = () => {
   const inputReais = document.getElementById("input-real").value;
@@ -34,7 +35,14 @@ const convertValues = () => {
       style: "decimal",
       currency: "BTC",
       maximumFractionDigits: 8,
-  }).format(inputReais * bitcoin);
+    }).format(inputReais * bitcoin);
+  }
+
+  if (select.value === "¥ Renminbi chinês") {
+    currencyValueText.innerHTML = new Intl.NumberFormat('zh-CN', {
+      style: 'currency',
+      currency: "CNY",
+    }).format(inputReais / renminbi);
   }
 
 };
@@ -45,17 +53,22 @@ changeCurrency = () => {
 
   if (select.value === "US$ Dólar americano") {
     currencyName.innerHTML = "Dólar americano";
-    currencyImg.src = "./assets/estados-unidos (1) 1.png";
+    currencyImg.src = "./assets/assets/estados-unidos (1) 1.png";
   }
 
   if (select.value === "€ Euro") {
     currencyName.innerHTML = "Euro";
-    currencyImg.src = "./assets/euro.png";
+    currencyImg.src = "./assets/assets/euro.png";
   }
 
   if (select.value === "₿ Bitcoin") {
     currencyName.innerHTML = "Bitcoin";
-    currencyImg.src = "./assets/bitcoin.png";
+    currencyImg.src = "./assets/assets/bitcoin.png";
+  }
+
+  if (select.value === "¥ Renminbi chinês") {
+    currencyName.innerHTML = "Renminbi chinês";
+    currencyImg.src = "./assets/assets/bandeira-da-china.png";
   }
 
   convertValues()
